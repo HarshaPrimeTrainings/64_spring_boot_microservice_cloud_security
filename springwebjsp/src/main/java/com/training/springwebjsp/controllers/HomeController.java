c1a752fb44ec88cbb12edaa880aad1fe1d046ad8
HarshaVardhan <athaluri.harsha@gmail.com>
2025-05-08 08:22:54 +0530

    webapp update

--- springwebjsp/src/main/java/com/training/springwebjsp/controllers/HomeController.java
+++ springwebjsp/src/main/java/com/training/springwebjsp/controllers/HomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.training.springwebjsp.model.User;
@@ -15,15 +16,15 @@ public class HomeController {
 	List<User> users = new ArrayList<>();
 	
 	public HomeController() {
-		users = List.of(new User(1, "vivek", "Hyd", "vivek@gmai.com"),
-				new User(2, "anand", "bang", "anand@gmai.com"),
-				new User(3, "monika", "chennai", "monika@gmai.com"),
-				new User(4, "raju", "pune", "raju@gmai.com"));
+		users.add(new User(1, "vivek", "Hyd", "vivek@gmai.com"));
+		users.add(new User(2, "anand", "bang", "anand@gmai.com"));
+		users.add(new User(3, "monika", "chennai", "monika@gmai.com"));
+		users.add(new User(4, "raju", "pune", "raju@gmai.com"));
 		
 	}
 	
 	
-	@RequestMapping("/users")
+	@RequestMapping(value = {"/","/users"})
 	public ModelAndView usersList() {
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("userlist",users );
@@ -37,6 +38,20 @@ public class HomeController {
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("msg", "Data Coming from Controller");
 		mav.setViewName("home");
+		return mav;
+	}
+	
+	@RequestMapping("/adduser")
+	public String addUser() {
+		return "adduser";
+	}
+	
+	@RequestMapping(value = "/save" ,method = RequestMethod.POST)
+	public ModelAndView saveuser(User u) {
+		users.add(u);
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("userlist",users);
+		mav.setViewName("users");
 		return mav;
 	}
 	
--- springwebjsp/src/main/webapp/WEB-INF/adduser.jsp
+++ springwebjsp/src/main/webapp/WEB-INF/adduser.jsp
@@ -7,7 +7,14 @@
 <title>Insert title here</title>
 </head>
 <body>
-
-
+<center>
+<form action="/save" method="post">
+UID:  <input type="text" name="uid"> </br>
+Name:  <input type="text" name="name"> </br>
+ADDR:  <input type="text" name="adress"></br>
+EMAIL: <input type="text" name="email"></br>
+<input type="submit" value="save">
+</form>
+</center>
 </body>
 </html>
