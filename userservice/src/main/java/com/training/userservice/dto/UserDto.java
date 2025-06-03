560c67a6ec4ee4f7f69c9ff9ca0e9b7d8f7bc1a2
HarshaVardhan <athaluri.harsha@gmail.com>
2025-06-03 08:22:44 +0530

    eureka service

---
+++ userservice/src/main/java/com/training/userservice/dto/UserDto.java
@@ -0,0 +1,44 @@
+package com.training.userservice.dto;
+
+import java.util.List;
+
+public class UserDto {
+	
+    private Integer uid;
+	private String uname;
+	private String address;
+	private String contact;
+	private List<OrderDto> orders;
+	
+	public Integer getUid() {
+		return uid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public List<OrderDto> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<OrderDto> orders) {
+		this.orders = orders;
+	}
+
+}
